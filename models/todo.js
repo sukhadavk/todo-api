b0ad5ab7a984c8c2193c9d12511d483e4890f2d3
Sukhada Karhade <sukhada.karhade@sigma-systems.com>
2016-02-12 07:41:54 +0530

    added todo's in DB

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTpes) {
+	return sequelize.define('todo', {
+		description: {
+			type: DataTpes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 250]
+			}
+		},
+		completed: {
+			type: DataTpes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	});
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -55,16 +56,22 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');;
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(404).send();
-	}
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(404).send();
+	// }
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
 
-	todos.push(body);
+	// todos.push(body);
 
-	res.json(body);
+	// res.json(body);
+
+	db.todo.create(body).then(function (todo) {
+		res.status(200).json(todo.toJSON());
+	}, function (e) {
+		return res.status(404).json(e);
+	}) ;
 });
 
 //DELETE - /todos/:id
@@ -127,6 +134,8 @@ app.put('/todos/:id', function(req, res) {
 	}
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port : ' + PORT);
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port : ' + PORT);
+	});
 });
