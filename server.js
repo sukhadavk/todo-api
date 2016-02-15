ceb5f58025ffa7b340fee4547218ca823c6fdc22
Sukhada Karhade <sukhada.karhade@sigma-systems.com>
2016-02-15 11:55:06 +0530

    suer-todo mapping

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ app.post('/users/login', function(req, res) {
 });
 
 db.sequelize.sync({
-	//force: true
+	force: true
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port : ' + PORT);
